7449d3ae9bd9655e7e87b8ab284c6f8674277fba
Ashok Madudru <ashokgirihari@gmail.com>
2014-11-07 18:36:10 +0530

    Input class is added

---
+++ PrimeNumbers/src/shok/prime/Input.java
@@ -0,0 +1,13 @@
+package shok.prime;
+
+public class Input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num=5;
+		GeneratePrime gen=new GeneratePrime();
+		gen.Get_prime_nos(num);
+	}
+
+}
